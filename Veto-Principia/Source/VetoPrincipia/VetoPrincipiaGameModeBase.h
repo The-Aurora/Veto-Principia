476378e047ea65d3d4789dd6c884ec75aeb0cf22
The-Aurora <aurora@matthewporuben.com>
2017-10-09 21:23:02 +0200

    Initial Commit

---
+++ Veto-Principia/Source/VetoPrincipia/VetoPrincipiaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VetoPrincipiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VETOPRINCIPIA_API AVetoPrincipiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
